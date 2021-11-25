e2380b4fadea8d86681ab0785098925980ac4fe2
Abady <afr.rom@gmail.com>
2021-11-25 13:09:55 +0300

    Enhance the script with column mapping and file name

--- xlsxcombine.py
+++ xlsxcombine.py
@@ -1,12 +1,22 @@
 import os
 import pandas as pd
+generated_filename = 'combined3.xlsx'
 cwd = os.path.abspath('')
 files = os.listdir(cwd)
 df = pd.DataFrame()
 for file in files:
     if file.endswith('.xlsx'):
-        dfs = pd.read_excel(file)
+        filedata = pd.read_excel(file)
+        x_row = filedata.columns[1]
+        y_row = filedata.columns[2]
+        no_row = filedata.columns[0]
+        dfs = pd.read_excel('template.xlsx')
+        dfs['x'] = filedata[x_row].values
+        dfs['y'] = filedata[y_row].values
+        dfs['#'] = filedata[no_row].values
         dfs['name'] = file
         df = df.append(dfs, ignore_index=True)
 df.head()
-df.to_excel('combined2.xlsx')
+if os.path.exists(generated_filename):
+  os.remove(generated_filename)
+df.to_excel(generated_filename)
